2b395a8779a0ceed485f31708879f9517890869a
Jon Kagan <darkspadez@silence.(none)>
2014-02-03 18:32:55 -0500

    Slimify
    get ghost ready for slim

--- slim.mk
+++ slim.mk
@@ -1,8 +1,5 @@
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
-
-# Enhanced NFC
-$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
+$(call inherit-product, vendor/slim/config/common_full_phone.mk)
+$(call inherit-product, vendor/slim/config/nfc_enhanced.mk)
 
 # Boot animation
 TARGET_SCREEN_WIDTH := 720
@@ -10,7 +7,7 @@ TARGET_SCREEN_HEIGHT := 1280
 
 # Release name
 PRODUCT_RELEASE_NAME := MOTO X
-PRODUCT_NAME := cm_ghost
+PRODUCT_NAME := slim_ghost
 
 $(call inherit-product, device/motorola/ghost/full_ghost.mk)
 
